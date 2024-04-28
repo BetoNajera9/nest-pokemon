5077d123866bdea89b196d3feea43982468b6a6f
BetoNajera <ing.betonajera@gmail.com>
2024-04-27 19:16:21 -0600

    :art: Format: Format seed module
    Format seed module

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -4,7 +4,7 @@ import { SeedService } from './seed.service';
 
 @Controller('seed')
 export class SeedController {
-  constructor(private readonly seedService: SeedService) { }
+  constructor(private readonly seedService: SeedService) {}
   @Get()
   executeSeed() {
     return this.seedService.executeSeed();
